4ea07204f282791b323e33a7b2f55434579a26b5
kubilus1 <mattkubilus@gmail.com>
2016-10-19 17:14:55 -0400

    Clarify a bit

--- cloud/softlayer/sl_metal.py
+++ cloud/softlayer/sl_metal.py
@@ -27,7 +27,7 @@ DOCUMENTATION = '''
 module: sl_metal
 short_description: Order a bare-metal instance on SoftLayer
 description:
-  - Create a bare-metal server in SoftLayer.  This technique may be used to other SoftLayer hardware components such as storage, firewalls, and other 'packages'.  
+  - Create a bare-metal server in SoftLayer.  This technique may be used to order other SoftLayer hardware components such as storage, firewalls, and other 'packages', this module will focus BARE_METAL_CPU (servers), however.  
   - Options and values are highly dependant on the choice of package that is used.  Because of this, there will be a set of standard options, and then for the choice of a package type there will be an additional setup of options specific to that package.
   - The following is a list of standard options and options for a typical pkgid 253 (DUAL_E52600_4_DRIVES) system.
 version_added: "0.0"
